b46871866f7d4c8699c3e50091f28a695e76ef96
Smitpatel0409 <106525445+Smitpatel0409@users.noreply.github.com>
2023-09-12 21:36:19 +0530

    index.js

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ class BankAccount {
         this.balance = 0;
     }
 
+    performAction(action, amount) {
+        switch (action) {
+            case 'deposit':
+                try {
+                    this.deposit(amount);
+                    return `Deposited $${amount} successfully. New balance: $${this.getBalance()}`;
+                } catch (error) {
+                    return error.message;
+                }
+                break;
+            case 'withdraw':
+                try {
+                    this.withdraw(amount);
+                    return `Withdrawn $${amount} successfully. New balance: $${this.getBalance()}`;
+                } catch (error) {
+                    return error.message;
+                }
+                break;
+            case 'balance':
+                return `Current balance: $${this.getBalance()}`;
+            default:
+                return 'Invalid action';
+        }
+    }
+
     deposit(amount) {
         if (amount <= 0) {
             throw new Error('Invalid amount for deposit');
@@ -30,51 +55,29 @@ class BankAccount {
 const accountNameInput = document.getElementById('accountName');
 const accountTypeSelect = document.getElementById('accountType');
 const amountInput = document.getElementById('amount');
-const depositButton = document.getElementById('deposit');
-const withdrawButton = document.getElementById('withdraw');
-const balanceButton = document.getElementById('balance');
+const actionButtons = document.querySelectorAll('.action-button');
 const messageElement = document.getElementById('message');
 
 let currentAccount;
-
-depositButton.addEventListener('click', () => {
-    try {
-        const amount = parseFloat(amountInput.value);
-        if (isNaN(amount)) {
-            throw new Error('Invalid amount');
-        }
-        currentAccount.deposit(amount);
-        messageElement.textContent = `Deposited $${amount} successfully. New balance: $${currentAccount.getBalance()}`;
-    } catch (error) {
-        messageElement.textContent = error.message;
-    } finally {
-        amountInput.value = '';
-    }
-});
-
-withdrawButton.addEventListener('click', () => {
-    try {
-        const amount = parseFloat(amountInput.value);
-        if (isNaN(amount)) {
-            throw new Error('Invalid amount');
-        }
-        currentAccount.withdraw(amount);
-        messageElement.textContent = `Withdrawn $${amount} successfully. New balance: $${currentAccount.getBalance()}`;
-    } catch (error) {
-        messageElement.textContent = error.message;
-    } finally {
-        amountInput.value = '';
-    }
-});
-
-balanceButton.addEventListener('click', () => {
-    const balance = currentAccount.getBalance();
-    messageElement.textContent = `Current balance: $${balance}`;
-});
 
 accountNameInput.addEventListener('change', () => {
     const accountName = accountNameInput.value.trim();
     const accountType = accountTypeSelect.value;
     currentAccount = new BankAccount(accountName, accountType);
     messageElement.textContent = `Account created for ${accountName} (${accountType}).`;
+});
+
+actionButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const action = button.getAttribute('data-action');
+        const amount = parseFloat(amountInput.value);
+        
+        let message = currentAccount.performAction(action, amount);
+        
+        messageElement.textContent = message;
+        
+        if (action === 'deposit' || action === 'withdraw') {
+            amountInput.value = '';
+        }
+    });
 });
